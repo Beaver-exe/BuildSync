45e84bd0a0a7f6a975095dd5c92cfb9be27f0bd3
Beaver-exe <julianserhan1@gmail.com>
2026-06-09 11:51:11 +1000

    Updaed ProjectCategoryTable

--- Models/ProjectCategory.cs
+++ Models/ProjectCategory.cs
@@ -5,6 +5,6 @@ public class ProjectCategory
     public int ProjectCategoryId { get; set; }
     public string CategoryName { get; set; } = string.Empty;
     public int ProjectId { get; set; }
-    public string ProjectName { get; set; } = string.Empty;
+    public Project Project { get; set; } = null!;
     public List<Document> Documents { get; set; } = new();
 }
