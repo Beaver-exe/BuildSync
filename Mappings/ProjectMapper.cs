b4fbd1e62ef27eb97cccfb56fad1aad5d4dc2804
Beaver-exe <julianserhan1@gmail.com>
2026-06-10 13:22:15 +1000

    Implimented ProjectMapper class

---
+++ Mappings/ProjectMapper.cs
@@ -0,0 +1,55 @@
+using BuildSync.DTOs.Project;
+using BuildSync.DTOs.Users;
+using BuildSync.Models;
+
+namespace BuildSync.Mappings;
+
+public static class ProjectMapper
+{
+    public static ProjectDto ToDto(Project project)
+    {
+        return new ProjectDto
+        {
+            ProjectId = project.ProjectId,
+            ProjectName = project.ProjectName,
+            ProjectDescription = project.ProjectDescription,
+            ProjectOwnerId = project.ProjectOwnerId,
+
+            ProjectOwner = new UserDto
+            {
+                UserId = project.ProjectOwner.UserId,
+                FirstName = project.ProjectOwner.FirstName,
+                LastName = project.ProjectOwner.LastName,
+                Profession = project.ProjectOwner.Profession,
+                Email = project.ProjectOwner.Email
+            },
+
+            ProjectUsers = project.ProjectUsers.Select(pu => new ProjectUserDto
+            {
+                FirstName = pu.User.FirstName,
+                LastName = pu.User.LastName,
+                Email = pu.User.Email,
+                Role = pu.Role
+
+            }).ToList(),
+            
+            Categories = project.Categories.Select(c => new ProjectCategoryDto
+            {
+                ProjectCategoryId = c.ProjectCategoryId,
+                CategoryName = c.CategoryName
+            }).ToList()
+        };
+    }
+
+    public static GetProjectDto ToSummaryDto(Project project, int currentUserId)
+    {
+        return new GetProjectDto
+        {
+            ProjectId = project.ProjectId,
+            ProjectName = project.ProjectName,
+            ProjectDescription = project.ProjectDescription,
+            IsOwner = project.ProjectOwnerId == currentUserId,
+            IsAdmin = project.ProjectUsers.Any(u => u.UserId == currentUserId && u.Role == "Admin")
+        };
+    }
+}
--- Services/ProjectService.cs
+++ Services/ProjectService.cs
@@ -1,7 +1,7 @@
 using BuildSync.Data;
 using BuildSync.DTOs.Project;
-using BuildSync.DTOs.Users;
 using BuildSync.Models;
+using BuildSync.Mappings;
 using Microsoft.EntityFrameworkCore;
 
 namespace BuildSync.Services;
@@ -20,6 +20,7 @@ public class ProjectService
     public async Task<GetProjectsResponse> RetrieveProjectsAsync()
     {
         var userId = _currentUser.UserId;
+
         var projects = await _db.Projects
             .Include(p => p.ProjectUsers)
             .Where(p =>
@@ -30,24 +31,14 @@ public class ProjectService
         var response = new GetProjectsResponse
         {   
             Message = "Projects retrieval successful",
-            
-            Projects = projects.Select(p => new GetProjectDto
-            {
-                ProjectId = p.ProjectId,
-                ProjectName = p.ProjectName,
-                ProjectDescription = p.ProjectDescription,
-                IsOwner = p.ProjectOwnerId == userId,
-                IsAdmin = p.ProjectUsers.Any(u =>
-                    u.UserId == userId && u.Role == "Admin")
-
-            }).ToList()
+            Projects = projects.Select(p => ProjectMapper.ToSummaryDto(p, userId)).ToList()
         };
 
         return response;
     }
 
     public async Task<ProjectDto?> RetrieveProjectAsync(int projectId)
-    {  
+    {
         var userId = _currentUser.UserId;
 
         var project = await _db.Projects
@@ -55,51 +46,14 @@ public class ProjectService
             .Include(p => p.ProjectUsers)
                 .ThenInclude(pu => pu.User)
             .Include(p => p.Categories)
-            .Where(p => 
+            .FirstOrDefaultAsync(p =>
                 p.ProjectId == projectId &&
-                p.ProjectUsers.Any(u => u.UserId == userId))
-            .FirstOrDefaultAsync();
+                p.ProjectUsers.Any(u => u.UserId == userId));
 
-        if (project == null)
-        {
-            return null;
-        }
-
-        var projectDto = new ProjectDto
-        {
-            ProjectId = project.ProjectId,
-            ProjectName = project.ProjectName,
-            ProjectDescription = project.ProjectDescription,
-            ProjectOwnerId = project.ProjectOwnerId,
-
-            ProjectOwner = new UserDto
-            {
-                UserId = project.ProjectOwner.UserId,
-                FirstName = project.ProjectOwner.FirstName,
-                LastName = project.ProjectOwner.LastName,
-                Profession = project.ProjectOwner.Profession,
-                Email = project.ProjectOwner.Email
-            },
-
-            ProjectUsers = project.ProjectUsers.Select(pu => new ProjectUserDto
-            {
-                FirstName = pu.User.FirstName,
-                LastName = pu.User.LastName,
-                Email = pu.User.Email,
-                Role = pu.Role
-
-            }).ToList(),
-            
-            Categories = project.Categories.Select(c => new ProjectCategoryDto
-            {
-                ProjectCategoryId = c.ProjectCategoryId,
-                CategoryName = c.CategoryName
-            }).ToList()
-        };
-
-        return projectDto;
+        return project == null
+            ? null
+            : ProjectMapper.ToDto(project);
     }
-
 
     public async Task<GetProjectDto> CreateProjectAsync(CreateProjectRequest request)
     {
@@ -121,15 +75,6 @@ public class ProjectService
         _db.Projects.Add(project);
         await _db.SaveChangesAsync();
 
-        var dto = new GetProjectDto
-        {
-            ProjectId = project.ProjectId,
-            ProjectName = project.ProjectName,
-            ProjectDescription = project.ProjectDescription,
-            IsOwner = project.ProjectOwnerId == userId,
-            IsAdmin = project.ProjectUsers.Any(u => u.UserId == userId && u.Role == "Admin")
-        };
-
-        return dto;
+        return ProjectMapper.ToSummaryDto(project, userId);
     }
 }
